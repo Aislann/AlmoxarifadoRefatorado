8e55bbbd4f86057df25912d9b3ed541c8866fd63
Aislan Oliveira de Carvalho <aislan.oliveira@souunit.com.br>
2024-05-22 13:14:02 -0300

    Entrega 05
    R24 - Otimização de uso de Banco de Dados

--- AlmoxarifadoAPI/Program.cs
+++ AlmoxarifadoAPI/Program.cs
@@ -1,13 +1,25 @@
 using AlmoxarifadoDomain.Models;
+using AlmoxarifadoInfrastructure.Data;
 using AlmoxarifadoInfrastructure.Data.Interfaces;
 using AlmoxarifadoInfrastructure.Data.Repositories;
 using AlmoxarifadoServices;
 using Microsoft.EntityFrameworkCore;
+using static AlmoxarifadoInfrastructure.Data.Repositories.ConexaoBancoRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<xAlmoxarifadoContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoDBSQL")));
+builder.Services.AddSingleton<IConexaoBanco, PrimeiraConexao>(sp =>
+    new PrimeiraConexao(builder.Configuration));
+builder.Services.AddSingleton<IConexaoBanco, ReplicaConexao>(sp =>
+    new ReplicaConexao(builder.Configuration));
+builder.Services.AddSingleton<DbContextAlmoxarifado>();
+
+builder.Services.AddDbContext<xAlmoxarifadoContext>((serviceProvider, options) =>
+{
+    var dbContextStrategy = serviceProvider.GetRequiredService<DbContextAlmoxarifado>();
+    var connectionString = dbContextStrategy.GetConnectionString();
+    options.UseSqlServer(connectionString);
+});
 
 builder.Services.AddScoped<xAlmoxarifadoContext>();
 
---
+++ AlmoxarifadoInfrastructure/Data/DbContextAlmoxarifado.cs
@@ -0,0 +1,29 @@
+﻿using AlmoxarifadoInfrastructure.Data.Interfaces;
+
+namespace AlmoxarifadoInfrastructure.Data
+{
+    public class DbContextAlmoxarifado
+    {
+        private readonly IConexaoBanco _primeiraConexaco;
+        private readonly IConexaoBanco _replicaConexaco;
+
+        public DbContextAlmoxarifado(IConexaoBanco primeiraConexaco, IConexaoBanco replicaConexaco)
+        {
+            _primeiraConexaco = primeiraConexaco;
+            _replicaConexaco = replicaConexaco;
+        }
+
+        public string GetConnectionString()
+        {
+            try
+            {
+                return _primeiraConexaco.GetConnectionString();
+            }
+            catch
+            {
+                return _replicaConexaco.GetConnectionString();
+            }
+        }
+    }
+
+}
